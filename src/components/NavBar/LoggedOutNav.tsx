e2f21b149d583bbf7c00d819a398d7bed04e7353
Yogev Lahyani <yogevlahyani@gmail.com>
2021-08-18 07:58:58 +0300

    Add translations

--- src/components/NavBar/LoggedOutNav.tsx
+++ src/components/NavBar/LoggedOutNav.tsx
@@ -1,36 +1,33 @@
 import React from "react";
 import { Button, Flex, Link } from "@chakra-ui/react";
+import useTranslation from "next-translate/useTranslation";
 
-export const LoggedOutNav = () => (
-  <Flex align="center">
-    <Link
-      textTransform="uppercase"
-      _hover={{ textDecoration: "none", color: "tomato" }}
-      mx={3}
-      href="/instructors"
-    >
-      Start coaching
-    </Link>
-    <Link
-      textTransform="uppercase"
-      _hover={{ textDecoration: "none", color: "tomato" }}
-      mx={3}
-      href="/sign-in"
-    >
-      Sign In
-    </Link>
-    <Button
-      as="a"
-      textTransform="uppercase"
-      bg="tomato"
-      color="white"
-      border="1px solid tomato"
-      _hover={{ textDecoration: "none", color: "tomato", bg: "white" }}
-      mx={3}
-      size="sm"
-      href="/sign-up"
-    >
-      Sign Up
-    </Button>
-  </Flex>
-);
+export const LoggedOutNav = () => {
+  const { t } = useTranslation("common");
+
+  return (
+    <Flex align="center">
+      <Link
+        textTransform="uppercase"
+        _hover={{ textDecoration: "none", color: "tomato" }}
+        mx={3}
+        href="/sign-in"
+      >
+        {t("Sign In")}
+      </Link>
+      <Button
+        as="a"
+        textTransform="uppercase"
+        bg="tomato"
+        color="white"
+        border="1px solid tomato"
+        _hover={{ textDecoration: "none", color: "tomato", bg: "white" }}
+        mx={3}
+        size="sm"
+        href="/sign-up"
+      >
+        {t("Sign Up")}
+      </Button>
+    </Flex>
+  );
+};
