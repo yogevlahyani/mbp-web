4c3278fd705689e6ccfc44b2b20265ebf643c145
Yogev Lahyani <yogevlahyani@gmail.com>
2021-08-27 19:18:35 +0300

    Add Netlify CMS (#2)
    * Add Netlify CMS
    
    * Small fixes
    
    * Small changes

--- config.ts
+++ config.ts
@@ -6,6 +6,42 @@ const config = {
     backend: {
       baseUrl: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
     },
+    netlifyCMS: {
+      cms_manual_init: true,
+      backend: {
+        name: "github",
+        repo: "yogevlahyani/mbp-web",
+        branch: "main",
+      },
+      publish_mode: "editorial_workflow",
+      media_folder: "public/images/uploads",
+      public_folder: "images",
+      collections: [
+        {
+          name: "pages",
+          label: "Pages",
+          files: [
+            {
+              label: "Home",
+              name: "home",
+              file: "content/pages/home.md",
+              fields: [
+                {
+                  label: "Title",
+                  name: "title",
+                  widget: "string",
+                },
+                {
+                  label: "Description",
+                  name: "description",
+                  widget: "markdown",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
   },
 };
 
