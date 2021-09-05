7bc35664134d8dc0d65bee0e16e4c33b7f9e3ed8
Katriel <createappltd@gmail.com>
2021-09-05 16:08:18 +0300

    Added language support to ContentHeader and fixed padding bugs

--- src/components/Home/ContentHeader.tsx
+++ src/components/Home/ContentHeader.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import useTranslation from 'next-translate/useTranslation'
+import Link from 'next/link'
 import {
 	Container,
 	Box,
@@ -10,50 +11,44 @@ import {
 	extendTheme,
 	Button,
 	ButtonGroup,
-} from '@chakra-ui/react';
-
-const theme = extendTheme({
-	textStyles: {
-		h1: {
-			fontWeight: 'bold',
-		},
-	},
-});
+} from '@chakra-ui/react'
 
 export const ContentHeader = () => {
+	const { t } = useTranslation('common')
+
 	return (
 		<>
-			<Container mb="20" maxW="5xl" pt="-10" centerContent>
+			<Container mb='20' maxW='5xl' centerContent>
 				<Flex
-					h="600px"
-					w="100%"
-					align="flex-end"
-					p="10"
-					bgGradient="linear(to-l, #1A74E266, #00544800)"
-					borderBottomLeftRadius="40px"
-					borderBottomRightRadius="40px"
+					h='600px'
+					w='100%'
+					align='flex-end'
+					p='10'
+					bgGradient='linear(to-l, #1A74E266, #00544800)'
+					borderBottomLeftRadius='40px'
+					borderBottomRightRadius='40px'
 				>
 					<Box>
-						<Text fontWeight="bold" as="h1" fontSize="4xl">
-							להתאמן וללמוד מהטובים ביותר
+						<Text fontWeight='bold' as='h1' fontSize='4xl'>
+							{t('Workout and learn from the best')}
 						</Text>
-						<Text mt="2" mb="4">
-							לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית.
+						<Text mt='2' mb='4'>
+							{t('Loren ipsum dolor')}
 						</Text>
 						<Button
-							fontSize="21px"
-							fontWeight="normal"
-							size="lg"
-							border="2px"
-							pl="10"
-							pr="10"
-							colorScheme="blue"
+							fontSize='21px'
+							fontWeight='normal'
+							size='lg'
+							border='2px'
+							pl='10'
+							pr='10'
+							colorScheme='blue'
 						>
-							למד עוד
+							{t('Learn more')}
 						</Button>
 					</Box>
 				</Flex>
 			</Container>
 		</>
-	);
-};
+	)
+}
