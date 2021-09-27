12df87e3e2d4316d564e425d075de97c413d97ef
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2021-09-28 01:22:44 +0300

    Use VERCEL_URL

--- src/components/EnvBadge.tsx
+++ src/components/EnvBadge.tsx
@@ -6,19 +6,11 @@ interface Props {}
 
 export const EnvBadge: React.FC<Props> = ({}) => {
   const { environment, isProduction } = config;
-  console.log('isProduction', isProduction);
-  console.log('environment', environment);
-  console.log('process.env.APP_ENV', process.env.VERCEL_ENV);
 
-  const colorSchema = useMemo(() => {
-    console.log('colorSchema environment', environment);
-    console.log('colorSchema environment.toLowerCase()', environment.toLowerCase());
-    console.log('environment.toLowerCase() === staging', environment.toLowerCase() === 'staging');
-
-    return environment.toLowerCase() === 'staging' ? 'green' : 'red';
-  }, [environment]);
-
-  console.log('colorSchema', colorSchema);
+  const colorSchema = useMemo(
+    () => (environment.toLowerCase() === 'preview' ? 'green' : 'red'),
+    [environment],
+  );
 
   if (isProduction) {
     return null;
