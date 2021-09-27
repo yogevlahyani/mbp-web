a64fcb0dd365d904eec15d1337d887785d4be03e
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2021-09-28 00:28:25 +0300

    Add Dev Badge

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 const config = {
-  environment: process.env.NEXT_PUBLIC_VERCEL_ENV || 'local',
-  isProduction: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production',
+  environment: process.env.NODE_ENV || 'local',
+  isProduction: process.env.NODE_ENV === 'production',
   providers: {
     hasura: {
       graphqlUrl: process.env.HASURA_GRAPHQL_URL,
