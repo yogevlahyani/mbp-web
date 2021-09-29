e472897173970b489264a6f6c81ee48ef6ef5dc7
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2021-09-29 19:24:45 +0300

    Fix GoBackButton

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { chakra, IconButton, IconButtonProps } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { ArrowBendUpRight } from 'phosphor-react';
 
 interface Props extends Omit<IconButtonProps, 'aria-label'> {}
@@ -7,13 +8,15 @@ interface Props extends Omit<IconButtonProps, 'aria-label'> {}
 const ArrowBendUpRightIcon = chakra(ArrowBendUpRight);
 
 export const GoBackButton: React.FC<Props> = (iconProps) => {
+  const { push } = useRouter();
+
   return (
     <IconButton
       {...iconProps}
       aria-label="Go Back"
       icon={<ArrowBendUpRightIcon size={20} />}
       size="md"
-      onClick={() => (window.location.href = '/')}
+      onClick={() => push('/')}
       variant="solid"
       colorScheme="twitter"
     />
