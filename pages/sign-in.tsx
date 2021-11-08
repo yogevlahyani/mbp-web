f437260af8ab464e27f4f28752cba3c49f4052dd
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2021-11-08 12:49:47 +0200

    getServerSideProps redirection

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { Box } from '@chakra-ui/react';
 import { SignInComponent } from '../src/components/Authentication/SignInComponent';
 import { getSession } from '../lib/session';
 
-interface Props {
-  loggedIn?: boolean;
-}
-
-export default function SignIn({ loggedIn }: Props) {
-  const { replace } = useRouter();
-
-  if (loggedIn) {
-    return replace('/');
-  }
-
+export default function SignIn() {
   return (
     <>
       <Head>
@@ -34,9 +23,17 @@ export default function SignIn({ loggedIn }: Props) {
 export async function getServerSideProps(ctx: any) {
   const session = await getSession(ctx.req, ctx.res);
 
+  if (session.user && session.token) {
+    return {
+      props: {},
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    };
+  }
+
   return {
-    props: {
-      loggedIn: session.user && session.token,
-    },
+    props: {},
   };
 }
