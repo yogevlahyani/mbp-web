b6d8d567e1cc7e20cad0f998948c22433a5e1728
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2021-11-08 12:45:48 +0200

    Only client-side redirection

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { Box } from '@chakra-ui/react';
 import { SignInComponent } from '../src/components/Authentication/SignInComponent';
 import { getSession } from '../lib/session';
-import { useRouter } from 'next/router';
 
 interface Props {
   loggedIn?: boolean;
@@ -34,21 +34,9 @@ export default function SignIn({ loggedIn }: Props) {
 export async function getServerSideProps(ctx: any) {
   const session = await getSession(ctx.req, ctx.res);
 
-  if (session.user && session.token) {
-    return {
-      props: {
-        loggedIn: true,
-      },
-      redirect: {
-        permanent: false,
-        destination: '/',
-      },
-    };
-  }
-
   return {
     props: {
-      loggedIn: false,
+      loggedIn: session.user && session.token,
     },
   };
 }
